7874d1652dcfd0829225434d7f4fb1ce788499d7
Serafli <galka0168@gmail.com>
2026-04-07 01:33:13 +0700

    lab2.1

---
+++ lab3/script.js
@@ -0,0 +1,40 @@
+// const name1 = "John";
+// let admin = name1;
+// alert(admin);
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// alert(Number(a) + Number(b));
+
+// for (let i = 2; i < 11; i++) {
+//     if (i%2==0)
+//         alert(i)
+// }
+
+// let i = 0;
+// while (i<3){
+//     alert( `number ${i}!` );
+//     i++;
+// }
+
+// let number = prompt("Введите число больше 100", "");
+// while (number !== null && number <= 100){
+//     number = prompt("Введите число больше 100", "");
+// }
+// alert(number)
+
+
+// function isPrime(num) {
+//   if (num <= 1) return false;
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) return false; 
+//   }
+//   return true; 
+// }
+
+// let number1 = 10;
+// for (let i = 1; i < number1; i++){
+//     if (isPrime(i))
+//         console.log(i);
+// }
+
